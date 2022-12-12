884a1f26f83c083fff1f4343df7cfec741c01ef5
Дубовик Юлия Викторовна <8-218@uk.armtek.local>
2022-12-12 12:33:37 +0300

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
+import { SelectorComponent } from './components/selector/selector.component';
+import { OrganizerComponent } from './components/organizer/organizer.component';
+import {MomentPipe} from "./shared/moment.pipe";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent,
+    SelectorComponent,
+    OrganizerComponent,
+    MomentPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/moment.pipe.ts
@@ -0,0 +1,15 @@
+import {Pipe, PipeTransform} from "@angular/core";
+import * as moment from 'moment';
+
+@Pipe({
+  name: 'moment',
+  pure: false
+})
+
+export class MomentPipe implements PipeTransform {
+  transform(m: moment.Moment | null, format: string = 'MMMM YYYY'): string {
+    if (!m) return '';
+    return m.format(format)
+  }
+
+}
